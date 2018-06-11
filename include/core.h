5a75d9093884c9060f577a3e036a66859bd0e6c3
Quentin_Gasparotto <quentin.gasparotto@epitech.eu>
2018-06-11 14:20:45 +0200

    Changing function.instruction.linkData parameters order

--- include/core.h
+++ include/core.h
@@ -206,7 +206,7 @@ inline namespace core
             void onEntryPointSet(std::function<void(EntityID, InstructionID)> const &then);
             void onSetEntryPointError(std::function<void(EntityID, InstructionID, QString)> const &error);
 
-            void linkData(EntityID function, InstructionID instruction, QString const &inputName, InstructionID fromInstruction, QString const &output);
+            void linkData(EntityID function, InstructionID from, QString const &output, InstructionID to, QString const &input);
             void onDataLinked(std::function<void(EntityID, InstructionID, QString, InstructionID, QString)> const &then);
             void onLinkDataError(std::function<void(EntityID, InstructionID, QString, InstructionID, QString, QString)> const &error);
 
